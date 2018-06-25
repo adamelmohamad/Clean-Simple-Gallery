0447e595d74e013feb48ef739c680efb0b87e598
adamelmohamad <37544116+adamelmohamad@users.noreply.github.com>
2018-06-25 16:36:07 +1000

    Add files via upload

---
+++ Gallery/js/main.js
@@ -0,0 +1,20 @@
+function original(){
+  var image = document.getElementById("current");
+  var image1 = document.getElementById("gle63");
+  image.src = "img/gle63.jpg";
+}
+
+function change1(){
+  var image = document.getElementById("current");
+  image.src = "img/audi.jpg";
+}
+
+function change2(){
+  var image = document.getElementById("current");
+  image.src = "img/bentley.jpg";
+}
+
+function change3(){
+  var image = document.getElementById("current");
+  image.src = "img/rs6.jpg";}
+
